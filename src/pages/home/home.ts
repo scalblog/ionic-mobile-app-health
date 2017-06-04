6de7ec676950a55c6f7a4789f72f82e7f6d658f0
scalblog <scalblog@yahoo.fr>
2017-06-04 19:03:59 +0200

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+//pages
+import { PlanningPage } from '../planning/planning';
+import { AboutPage } from '../about/about';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,4 +15,11 @@ export class HomePage {
 
   }
 
+  private showPlanning() {
+  	this.navCtrl.push(PlanningPage);
+  }
+
+  private showApropos() {
+  	this.navCtrl.push(AboutPage);
+  }
 }
---
+++ src/pages/reseautage/reseautage.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-reseautage',
+  templateUrl: 'reseautage.html'
+})
+export class ReseautagePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
